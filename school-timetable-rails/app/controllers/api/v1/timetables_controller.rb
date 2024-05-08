1c85374345b5a362d9c8c1af5e6135c568546430
champaya <hatton9619@yahoo.co.jp>
2024-05-08 18:52:24 +0900

    API作成（授業一覧取得, 時間割登録, 時間割削除）
    Rails
    　・授業一覧取得API作成
    　・時間割取得APIのインスタンス変数をローカル変数に変更
    　・時間割登録API作成
    　・時間割削除API作成

--- school-timetable-rails/app/controllers/api/v1/timetables_controller.rb
+++ school-timetable-rails/app/controllers/api/v1/timetables_controller.rb
@@ -6,12 +6,29 @@ module Api
       # @todo rails標準の機能でSQLを代替できないか検討
       # GET /api/v1/timetables/:id 対象ユーザの時間割を取得する
       def show
-        @timetable = Timetable.select('timetables.day_of_week, timetables.time, timetables.period, lectures.lecture_id, lectures.lecture_name, lectures.credit_count, teachers.teacher_name ')
-                              .joins('INNER JOIN lectures ON timetables.lecture_id = lectures.lecture_id INNER JOIN teachers ON lectures.teacher_id = teachers.teacher_id ')
-                              .where(
-                                'timetables.user_id = ?', params[:id]
-                              )
-        render json: { status: 'SUCCESS', message: 'Loaded timetables', data: @timetable }, status: :ok
+        timetable = Timetable.select('timetables.day_of_week, timetables.time, timetables.period, lectures.lecture_id, lectures.lecture_name, lectures.credit_count, teachers.teacher_name ')
+                             .joins('INNER JOIN lectures ON timetables.lecture_id = lectures.lecture_id INNER JOIN teachers ON lectures.teacher_id = teachers.teacher_id ')
+                             .where(
+                               'timetables.user_id = ?', params[:id]
+                             )
+        render json: { status: 'SUCCESS', message: 'Loaded timetables', data: timetable }, status: :ok
+      end
+
+      # POST /api/v1/timetables 対象ユーザに授業を登録する
+      def create
+        args = ['insert timetables (user_id,	day_of_week, `time`, period, lecture_id, created_at, updated_at)
+                 select ?, lectures.day_of_week, lectures.`time`, lectures.period, lectures.lecture_id , lectures.created_at, lectures.updated_at from lectures where lecture_id = ?',
+                params[:user_id], params[:lecture_id]]
+        sql = ActiveRecord::Base.send(:sanitize_sql_array, args)
+        ActiveRecord::Base.connection.execute(sql)
+      end
+
+      # DELETE /api/v1/timetables/:id 対象ユーザの対象授業を削除する
+      def destroy
+        args = ['delete from timetables where user_id = ? and lecture_id = ?',
+                params[:id], params[:lecture_id]]
+        sql = ActiveRecord::Base.send(:sanitize_sql_array, args)
+        ActiveRecord::Base.connection.execute(sql)
       end
     end
   end
